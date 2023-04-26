ede1762ef5f7cec6b3a4c782b997fb9382ad0ff6
unknown <sohailshahidi1435@gmail.com>
2023-04-26 19:59:20 +0430

    Make Person Class inherits from Nameable Class

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative 'nameable.rb'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
@@ -17,5 +19,9 @@ class Person
 
   def of_age?
     @age >= 18
+  end
+
+  def correct_name
+    @name
   end
 end
