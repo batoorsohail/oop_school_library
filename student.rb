10073a06523a970f0e23011d903e9f39ef372888
unknown <sohailshahidi1435@gmail.com>
2023-04-28 00:00:24 +0430

    Fix Linters error

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
