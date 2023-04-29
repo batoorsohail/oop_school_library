a03104e3785604bc105b905ed73c78862d29b8ed
unknown <sohailshahidi1435@gmail.com>
2023-04-29 09:41:06 +0430

    Fix Linters error

--- person.rb
+++ person.rb
@@ -35,7 +35,7 @@ class Person < Nameable
   end
 end
 
-person = Person.new( 22, 'maximilianus')
+person = Person.new(22, 'maximilianus')
 person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
