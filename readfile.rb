441a71f7d4ee14768329440e9b319df680bd253b
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-05-03 16:55:10 +0300

    Update readfile.rb

--- readfile.rb
+++ readfile.rb
@@ -3,15 +3,15 @@ require 'fileutils'
 
 class ReadFile
   def self.create_directory
-    return if File.directory?('./store')
-    FileUtils.mkdir_p('./store')
+    return if File.directory?('./data-storage')
+    FileUtils.mkdir_p('./data-storage')
   end
 
   def self.load_books
     books = []
     create_directory
-    File.write('./store/books.json', []) unless File.exist?('./store/books.json')
-    records = JSON.parse(File.read('./store/books.json'))
+    File.write('./data-storage/books.json', []) unless File.exist?('./data-storage/books.json')
+    records = JSON.parse(File.read('./data-storage/books.json'))
     records.each { |record| books << Book.new(record['title'], record['author']) }
     books
   end
@@ -19,8 +19,8 @@ class ReadFile
   def self.load_people
     people = []
     create_directory
-    File.write('./store/people.json', []) unless File.exist?('./store/people.json')
-    records = JSON.parse(File.read('./store/people.json'))
+    File.write('./data-storage/people.json', []) unless File.exist?('./data-storage/people.json')
+    records = JSON.parse(File.read('./data-storage/people.json'))
     records.each do |record|
       person = if record['type'] == 'Student'
                  Student.new(record['age'],
@@ -34,15 +34,15 @@ class ReadFile
       person.id = record['id']
       people << person
     end
-    
+
     people
   end
 
   def self.load_rentals(books, people)
     rentals = []
     create_directory
-    File.write('./store/rentals.json', []) unless File.exist?('./store/rentals.json')
-    records = JSON.parse(File.read('./store/rentals.json'))
+    File.write('./data-storage/rentals.json', []) unless File.exist?('./data-storage/rentals.json')
+    records = JSON.parse(File.read('./data-storage/rentals.json'))
     records.each do |record|
       book = books.select { |el| el.title == record['title'] }[0]
       person = people.select { |el| el.id == record['id'] }[0]
