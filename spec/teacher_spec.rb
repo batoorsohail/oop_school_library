b2e87c0d917aa8d7eca774cd014a881c1a56e459
unknown <sohailshahidi1435@gmail.com>
2023-05-04 21:03:31 +0430

    Update teacher_spec.rb

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -2,34 +2,29 @@ require_relative '../teacher'
 require_relative '../book'
 
 describe Teacher do
-  before :each do
-    @teacher = Teacher.new(49, 'Specialization1', 'Teacher1')
-  end
-
-  context 'teacher creation test' do
-    it 'should have' do
-      expect(@teacher.age).to eq 49
-      expect(@teacher.specialization).to eq 'Specialization1'
-      expect(@teacher.name).to eq 'Teacher1'
-      expect(@teacher.can_use_services?).to be true
+  context 'Testing Teacher class functionality' do
+    it 'Returns teacher details' do
+      teacher = Teacher.new(21, 'Ruby', 'Jhon')
+      expect(teacher.age).to eq 21
+      expect(teacher.name).to eq 'Jhon'
+      expect(teacher.specialization).to eq 'Ruby'
     end
-  end
 
-  context 'Teacher rental test' do
-    it 'should have' do
-      @teacher.add_rental('2023/04/27', Book.new('Title1', 'Author1'))
-      @teacher.add_rental('2023/04/27', Book.new('Title2', 'Author2'))
-      @teacher.add_rental('2023/04/27', Book.new('Title3', 'Author3'))
-      expect(@teacher.rentals.length).to be 3
-      expect(@teacher.rentals[0].book.title).to eq 'Title1'
-      expect(@teacher.rentals[1].book.author).to eq 'Author2'
-      expect(@teacher.rentals[2].book.author).to eq 'Author3'
+    it 'Return ¯(ツ)/¯ if the teacher plays hooky' do
+      teacher = Teacher.new(21, 'Ruby', 'Jhon')
+      expect(teacher.can_use_services?).to eq true
     end
-  end
 
-  context 'can use services test' do
-    it 'must return true' do
-      expect(@teacher.can_use_services?).to eq true
+    it 'returns a hash with the teacher details' do
+      teacher = Teacher.new(21, 'Ruby', 'Jhon')
+      id = teacher.instance_variable_get(:@id)
+      expect(teacher.to_h).to eq({
+                                   id: id,
+                                   age: 21,
+                                   name: 'Jhon',
+                                   specialization: 'Ruby',
+                                   rentals: []
+                                 })
     end
   end
 end
